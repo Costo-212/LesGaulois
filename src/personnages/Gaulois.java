fbcffc51fc55b84a4db515c84db41500de55534f
Radouane Akabi <kbr5187a@ensfsi.onmicrosoft.com>
2025-10-07 19:35:56 +0200

    fini tp1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,27 +1,51 @@
 package personnages;
 
 public class Gaulois {
-	private String nom;
-	private int force;
+    private String nom;
+    private int force;
+    private int effetPotion = 1;
 
-	public Gaulois(String nom, int force) {
-		this.nom = nom;
-		this.force = force;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "\"" + texte + "\"");
-	}
-	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
-	}
-	public int getForce() {
-		return force;
-	}
-	public static void main(String[] args) {
+    public Gaulois(String nom, int force) {
+        this.nom = nom;
+        this.force = force;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void parler(String texte) {
+        System.out.println(prendreParole() + "\"" + texte + "\"");
+    }
+
+    private String prendreParole() {
+        return "Le gaulois " + nom + " : ";
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void frapper(Romains romain) {
+        System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+        int forceCoup = (force * effetPotion) / 3;
+        romain.recevoirCoup(forceCoup);
+        if (effetPotion > 1) {
+            effetPotion--;
+        }
+    }
+
+    public void boirePotion(int forcePotion) {
+        effetPotion = forcePotion;
+        parler("Merci Druide, je sens que ma force est " + forcePotion + " fois décuplée.");
+    }
+
+    public String toString() {
+        return nom;
+    }
+
+    public static void main(String[] args) {
         Gaulois asterix = new Gaulois("Astérix", 8);
-        System.out.println(asterix);  // Affiche le nom complet de la classe + @ + adresse mémoire en hexadécimal
+        System.out.println(asterix);
     }
 }
--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,19 +1,33 @@
 package test_fonctionnel;
 
-import personnages.Gaulois;  
+import personnages.Gaulois;
+import personnages.Romains;
+import personnages.Druide;
 
 public class TestGaulois {
     public static void main(String[] args) {
         Gaulois asterix = new Gaulois("Astérix", 8);
-        
         Gaulois obelix = new Gaulois("Obélix", 16);
-        
-        System.out.println("Nom: " + asterix.getNom() + ", Force: " + asterix.getForce());
-        System.out.println("Nom: " + obelix.getNom() + ", Force: " + obelix.getForce());
-        
-        asterix.parler("Bonjour Obélix.");
-        obelix.parler("Bonjour Astérix. Ça te dirait d'aller chasser des sangliers ?");
-        asterix.parler("Oui très bonne idée.");
+        Romains minus = new Romains("Minus", 6);
+        Romains brutus = new Romains("Brutus", 14);
+        Druide panoramix = new Druide("Panoramix", 2);
+
+        System.out.println("Le Gaulois " + asterix + " : \"Bonjour Obélix.\"");
+        System.out.println("Le Gaulois " + obelix + " : \"Bonjour Astérix. Ca te dirais d'aller chasser des sangliers ?\"");
+        System.out.println("Le Gaulois " + asterix + " : \"Oui très bonne idée.\"");
+
+        System.out.println("Dans la forêt " + asterix + " et " + obelix + " tombent nez à nez sur le romain " + minus + ".");
+
+        for (int i = 0; i < 3; i++) {
+            asterix.frapper(minus);
+        }
+
+        panoramix.fabriquerPotion(4, 3);
+        panoramix.booster(obelix);
+        panoramix.booster(asterix);
+
+        for (int i = 0; i < 3; i++) {
+            asterix.frapper(brutus);
+        }
     }
-    
 }
