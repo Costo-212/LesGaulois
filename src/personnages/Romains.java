b5bae244c94ac002f2bfb6a8f72c837d9264fed9
Radouane Akabi <kbr5187a@ensfsi.onmicrosoft.com>
2025-11-04 18:46:31 +0100

    invariant pre et post cond

--- src/personnages/Romains.java
+++ src/personnages/Romains.java
@@ -5,9 +5,13 @@ public class Romains {
 	private int force;
 
 	public Romains(String nom, int force) {
-		this.nom = nom;
-		this.force = force;
+	    this.nom = nom;
+	    this.force = force;
+	    if (!isInvariantVerified()) {
+	        System.out.println("Attention : force invalide à la création !");
+	    }
 	}
+
 
 	public String getNom() {
 		return nom;
@@ -22,13 +26,38 @@ public class Romains {
 	}
 
 	public void recevoirCoup(int forceCoup) {
-		force -= forceCoup;
-		if (force < 1) {
-			force = 0;
-			parler("J'abandonne !");
-		} else {
-			parler("Aïe");
-		}
+	    if (forceCoup <= 0) {
+	        throw new IllegalArgumentException("La force du coup reçu doit être positive !");
+	    }
+
+	    int forceAvant = force;
+	    force -= forceCoup;
+
+	    if (force < 1) {
+	        force = 0;
+	        parler("J'abandonne !");
+	    } else {
+	        parler("Aïe");
+	    }
+
+	    if (!isInvariantVerified()) {
+	        System.out.println("Invariant violé : force négative !");
+	    }
+
+	    if (force > forceAvant) {
+	        throw new IllegalStateException("Postcondition violée : la force d'un Romain n'a pas diminué !");
+	    }
 	}
+
+
+	private boolean isInvariantVerified() {
+	    return force >= 0;
+	}
+	public static void main(String[] args) {
+	    Romains minus = new Romains("Minus", 6);
+	    System.out.println("Force de Minus après création : " + minus.force);
+	}
+
+
 }
 
