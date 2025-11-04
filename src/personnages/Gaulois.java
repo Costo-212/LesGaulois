25c60852e8e8663ca83a2c4105ccfc4fce00796d
Radouane Akabi <kbr5187a@ensfsi.onmicrosoft.com>
2025-11-04 19:53:15 +0100

    bug tp3 de merde

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,10 +1,15 @@
+import java.util.logging.Logger;
 package personnages;
 
 public class Gaulois {
+    private static final Logger logger = Logger.getLogger(Gaulois.class.getName());
     private String nom;
-    private int force;
-    private int effetPotion = 1;
+//    private int force;
+    
     private Village village;
+    private int force;
+     //TP3
+    private Equipement[] trophees = new Equipement[100]; //TP3
 
     public Gaulois(String nom, int force) {
         this.nom = nom;
@@ -20,38 +25,42 @@ public class Gaulois {
     }
 
     public void parler(String texte) {
-        System.out.println(prendreParole() + "\"" + texte + "\"");
+        logger.info(prendreParole() + "\"" + texte + "\"");
     }
 
-    private String prendreParole() {
-        return "Le gaulois " + nom + " : ";
-    }
+//    private String prendreParole() {
+//        return "Le gaulois " + nom + " : ";
+//    }
 
     public int getForce() {
         return force;
     }
 
-    public void frapper(Romains romain) {
-        System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-        int forceCoup = (force * effetPotion) / 3;
-        romain.recevoirCoup(forceCoup);
-        if (effetPotion > 1) {
-            effetPotion--;
-        }
-    }
+//    public void frapper(Romains romain) {
+//        System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+//        int forceCoup = (force * effetPotion) / 3;
+//        romain.recevoirCoup(forceCoup);
+//        if (effetPotion > 1) {
+//            effetPotion--;
+//        }
+//    }
 
     public void boirePotion(int forcePotion) {
-        effetPotion = forcePotion;
+
         parler("Merci Druide, je sens que ma force est " + forcePotion + " fois décuplée.");
     }
 
     public void sePresenter() {
-        if (village == null) {
-            parler("Bonjour, je m'appelle " + nom + ". Je voyage de villages en villages.");
+    	String message;
+    	message = "Bonjour, je m'apelle ";
+    	if (village == null) {
+        	
+
+            parler(message + nom + ". Je voyage de villages en villages.");
         } else if (village.getChef() == this) {
-            parler("Bonjour, je m'appelle " + nom + ". Je suis le chef du village " + village.getNom() + ".");
+            parler(message + nom + ". Je suis le chef du village " + village.getNom() + ".");
         } else {
-            parler("Bonjour, je m'appelle " + nom + ". J'habite le village " + village.getNom() + ".");
+            parler(message + nom + ". J'habite le village " + village.getNom() + ".");
         }
     }
 
@@ -64,4 +73,17 @@ public class Gaulois {
         Gaulois asterix = new Gaulois("Astérix", 8);
         System.out.println(asterix);
     }
+    //TP3
+    private String prendreParole() {
+    	return "Le gaulois " + nom + " : ";
+    	return texte;
+    	}
+    public void frapper(Romain romain) {
+        logger.info(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom() + ".");
+
+        for (int i = 0; trophees != null && i < trophees.length; i++, nb_trophees++) {
+            this.trophees[nb_trophees] = trophees[i];
+        }
+    }
+    
 }
--- src/personnages/Romains.java
+++ src/personnages/Romains.java
@@ -27,29 +27,29 @@ public class Romains {
         return "Le romain " + nom + " : ";
     }
 
-    public void recevoirCoup(int forceCoup) {
-        if (forceCoup <= 0) {
-            throw new IllegalArgumentException("La force du coup reçu doit être positive !");
-        }
-
-        int forceAvant = force;
-        force -= forceCoup;
-
-        if (force < 1) {
-            force = 0;
-            parler("J'abandonne !");
-        } else {
-            parler("Aïe");
-        }
-
-        if (!isInvariantVerified()) {
-            System.out.println("Invariant violé : force négative !");
-        }
-
-        if (force > forceAvant) {
-            throw new IllegalStateException("Postcondition violée : la force d'un Romain n'a pas diminué !");
-        }
-    }
+//    public void recevoirCoup(int forceCoup) {
+//        if (forceCoup <= 0) {
+//            throw new IllegalArgumentException("La force du coup reçu doit être positive !");
+//        }
+//
+//        int forceAvant = force;
+//        force -= forceCoup;
+//
+//        if (force < 1) {
+//            force = 0;
+//            parler("J'abandonne !");
+//        } else {
+//            parler("Aïe");
+//        }
+//
+//        if (!isInvariantVerified()) {
+//            System.out.println("Invariant violé : force négative !");
+//        }
+//
+//        if (force > forceAvant) {
+//            throw new IllegalStateException("Postcondition violée : la force d'un Romain n'a pas diminué !");
+//        }
+//    }
 
     private boolean isInvariantVerified() {
         return force >= 0;
@@ -86,5 +86,64 @@ public class Romains {
         minus.sEquiper(Equipement.BOUCLIER);
         minus.sEquiper(Equipement.CASQUE);
     }
+    //TP3 BUG
+    private String texte;
+    public Equipement[] recevoirCoup(int forceCoup) {
+    	Equipement[] equipementEjecte = null;
+    	forceCoup = CalculResistanceEquipement(forceCoup);
+    	force -= forceCoup;
+    	switch (force) {
+    	case 0:
+    	parler("Aïe");
+    	break;
+    	default:
+    	equipementEjecte = ejecterEquipement();
+    	parler("J'abandonne...");
+    	break;
+    	}
+    	return equipementEjecte;
+    	}
+    private int CalculResistanceEquipement(int forceCoup) {
+    	texte = "Ma force est de " + this.force + ", et la force du coup est de
+    	" + forceCoup;
+    	int resistanceEquipement = 0;
+    	if (!(nbEquipement == 0)) {
+    	texte += "\nMais heureusement, grace à mon équipement sa force est
+    	diminué de ";
+    	for (int i = 0; i < nbEquipement;) {
+    	if ((equipements[i] != null &&
+    	equipements[i].equals(Equipement.BOUCLIER)) == true) {
+    	resistanceEquipement += 8;
+    	} else {
+    	System.out.println("Equipement casque");
+    	resistanceEquipement += 5;
+    	}
+    	i++;
+    	}
+    	texte =+ resistanceEquipement + "!";
+    	}
+    	parler(texte);
+    	forceCoup -= resistanceEquipement;
+    	return forceCoup;
+    	}
+    	private Equipement[] ejecterEquipement() {
+    	Equipement[] equipementEjecte = new Equipement[nbEquipement];
+    	System.out.println("L'équipement de " + nom.toString() + " s'envole sous
+    	la force du coup.");
+    	//TODO
+    	int nbEquipementEjecte = 0;
+    	for (int i = 0; i < nbEquipement; i++) {
+    	if (equipements[i] == null) {
+    	continue;
+    	} else {
+    	equipementEjecte[nbEquipementEjecte] = equipements[i];
+    	nbEquipementEjecte++;
+    	equipements[i] = null;
+    	}
+    	}
+    	return equipementEjecte;
+    	}
+
+
 }
 
